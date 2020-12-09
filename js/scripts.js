a6a78981e01970451c9f7b967acf5dc02de44a8e
00010706 <75225176+00010706@users.noreply.github.com>
2020-12-09 21:41:40 +0500

    add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
-var tabNavs = document.querySelectorAll(".nav-tab");
-var tabPanes = document.querySelectorAll(".tab-pane");
+var tabNavs = document.querySelectorAll(".nav-tab"); //grabing elements in nav-tab
+var tabPanes = document.querySelectorAll(".tab-pane"); //grabing elements in tab-pane
 
-for (var i = 0; i < tabNavs.length; i++) {
+for (var i = 0; i < tabNavs.length; i++) { //endless loop for tabs
 
-  tabNavs[i].addEventListener("click", function(e){
+  tabNavs[i].addEventListener("click", function(e){ //event listener, which will work on every click
     e.preventDefault();
-    var activeTabAttr = e.target.getAttribute("data-tab");
+    var activeTabAttr = e.target.getAttribute("data-tab"); //variable to check activate tab
 
     for (var j = 0; j < tabNavs.length; j++) {
       var contentAttr = tabPanes[j].getAttribute("data-tab-content");
 
-      if (activeTabAttr === contentAttr) {
+      if (activeTabAttr === contentAttr) { //if users click on tab script will add class 'active' to dispaly it via css
         tabNavs[j].classList.add("active");
         tabPanes[j].classList.add("active"); 
-      } else {
+      } else { //then delete the previous one
         tabNavs[j].classList.remove("active");
         tabPanes[j].classList.remove("active");
       }
